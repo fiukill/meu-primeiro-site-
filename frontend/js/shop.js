f5708e65d917dcfe51f6e503f90db7baa3f2ca11
fiukill <julinhojgatinho132@gmail.com>
2026-01-06 20:20:51 -0300

    Delete frontend directory

--- frontend/js/shop.js
+++
@@ -1,6 +0,0 @@
-fetch('http://localhost:3000/api/products')
-.then(r=>r.json())
-.then(p=>{
- document.getElementById('products').innerHTML =
- p.map(x=>`<p>${x.name} - R$ ${x.price}</p>`).join('');
-});
